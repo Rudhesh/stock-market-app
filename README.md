7c2938d2701ffd1b60b3614d43f5e45776269222
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-11-22 16:51:34 +0100

    Set up redux, clean app 'creat-react-app' and creat dev branch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-#react
+# Stock market App
+
+### To install dependancies run:
+
+```zsh
+$ npm install
+```
+
+### To start the app locally run:
+
+```zsh
+$ npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from "react-redux";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ({ state }) => {
+    console.log("store from App: ", state);
+
+    return (
+        <div className="App">
+            <h1>Hello from App</h1>
+        </div>
+    );
+};
+
+function mapStateToProps(state) {
+    return {
+        state,
+    };
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
